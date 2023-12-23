6e12d9698584a9b60d655390a47eaa05855eacd5
Negan <huykgit98@gmail.com>
2023-12-24 03:39:58 +0700

    add tab bar to home screen

--- lib/home/home.dart
+++ lib/home/home.dart
@@ -1,100 +1,118 @@
 import 'package:devfin_ui_kit/utils.dart';
-import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) => DefaultTabController(
-        length: 5,
-        child: Scaffold(
-          body: Container(
-            width: double.maxFinite,
-            height: double.maxFinite,
-            decoration: const BoxDecoration(
-              gradient: LinearGradient(
-                colors: ColorsUtil.linearGradient,
-                begin: Alignment.centerLeft,
-                end: Alignment.centerRight,
-              ),
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage>
+    with SingleTickerProviderStateMixin {
+  late TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 5, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        body: Container(
+          width: double.maxFinite,
+          height: double.maxFinite,
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: ColorsUtil.linearGradient,
+              begin: Alignment.centerLeft,
+              end: Alignment.centerRight,
             ),
-            child: Column(
-              children: [
-                const TabBar(
-                  isScrollable: true,
-                  tabs: [
-                    Tab(text: 'Indices'),
-                    Tab(text: 'Stocks'),
-                    Tab(text: 'Cryptocurrency'),
-                    Tab(text: 'Commodities'),
-                    Tab(text: 'Currencies'),
+          ),
+          child: Column(
+            children: [
+              TabBar(
+                controller: _tabController,
+                isScrollable: true,
+                dividerColor: Colors.transparent,
+                indicator: ShapeDecoration(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20),
+                    side: BorderSide(color: Color(0xffd5d2d2), width: 1),
+                  ),
+                  gradient: LinearGradient(
+                    colors: ColorsUtil
+                        .linearGradientButton, // Change these colors to your desired gradient
+                  ),
+                ),
+                tabs: [
+                  _buildTabButton('Indices'),
+                  _buildTabButton('Stocks'),
+                  _buildTabButton('Cryptocurrency'),
+                  _buildTabButton('Commodities'),
+                  _buildTabButton('Currencies'),
+                ],
+              ),
+              Expanded(
+                child: TabBarView(
+                  controller: _tabController,
+                  children: [
+                    _buildTabContent(),
+                    _buildTabContent(),
+                    _buildTabContent(),
+                    _buildTabContent(),
+                    _buildTabContent(),
                   ],
                 ),
-                Expanded(
-                  child: TabBarView(
-                    children: [
-                      _buildTabContent(),
-                      _buildTabContent(),
-                      _buildTabContent(),
-                      _buildTabContent(),
-                      _buildTabContent(),
-                    ],
-                  ),
+              ),
+            ],
+          ),
+        ),
+      );
+
+  Widget _buildTabButton(String title) {
+    return Tab(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Text(title, style: const TextStyle(color: Colors.white)),
+      ),
+    );
+  }
+
+  Widget _buildTabContent() {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Container(
+        padding: const EdgeInsets.all(16.0),
+        decoration: BoxDecoration(
+          color: Colors.transparent.withOpacity(0.1),
+          border: Border.all(color: Colors.white, width: 1),
+          borderRadius: BorderRadius.circular(20),
+        ),
+        child: ListView.builder(
+          itemCount: 10,
+          itemBuilder: (context, index) => ListTile(
+            leading: FlutterLogo(),
+            title: const Text('Bitcoin', style: TextStyle(color: Colors.white)),
+            subtitle: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'BTC',
+                  style: TextStyle(color: Colors.white),
                 ),
               ],
             ),
           ),
         ),
-      );
-
-  Widget _buildTabContent() => ListView.builder(
-        itemCount: 10,
-        itemBuilder: (context, index) => ListTile(
-          leading: FlutterLogo(),
-          title: Text('Bitcoin'),
-          subtitle: Column(
-            mainAxisAlignment: MainAxisAlignment.center, // Add this line
-            children: [
-              Text('BTC'),
-              Container(
-                height: 100,
-                width: 200,
-                child: LineChart(
-                  LineChartData(
-                    gridData: FlGridData(show: false),
-                    titlesData: FlTitlesData(show: false),
-                    borderData: FlBorderData(show: false),
-                    lineBarsData: [
-                      LineChartBarData(
-                        spots: [
-                          FlSpot(0, 3),
-                          FlSpot(1, 2),
-                          FlSpot(2, 5),
-                          FlSpot(3, 3.1),
-                          FlSpot(4, 4),
-                        ],
-                        isCurved: true,
-                        dotData: FlDotData(show: false),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ],
-          ),
-          trailing: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.end,
-            children: [
-              Text('\$43,945.23'),
-              Text('-0.0010 (-0.09%)'),
-            ],
-          ),
-          contentPadding: EdgeInsets.symmetric(vertical: 10.0),
-          tileColor: Colors.white,
-          shape:
-              RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
-        ),
-      );
+      ),
+    );
+  }
 }
